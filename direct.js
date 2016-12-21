fabf2367bbb41a4c7ee6f7e0598bb21619e743d3
Daniele Scasciafratte <mte90net@gmail.com>
2016-12-21 11:54:58 +0100

    improving code

--- direct.js
+++ direct.js
@@ -2,19 +2,18 @@
 
 document.addEventListener("DOMNodeInserted", function(e) {
 	if(typeof e.srcElement.getElementsByTagName == "undefined") return;
-	var children = e.srcElement.getElementsByTagName("A");
-	for(var i=0; i<children.length; i++) {
-		if(children[i].target == "_blank") {
-			children[i].onmousedown = function() { return true; };
-		}
-	}
+        mousedown(e.srcElement.getElementsByTagName("A"));
 }, false);
 
-var children = document.getElementsByTagName("a");
-for(var i=0; i<children.length; i++) {
+mousedown(document.getElementsByTagName("a"));
+
+
+function mousedown(element) {
+    for(var i=0; i<children.length; i++) {
 	if(children[i].target == "_blank") {
 		children[i].onmousedown = function() { return true; };
 	}
+    }
 }
 
 })();
