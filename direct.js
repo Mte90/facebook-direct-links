a107c7a751293301daaee026145a12edba6f83f9
Daniele Scasciafratte <mte90net@gmail.com>
2018-04-28 00:22:25 +0200

    fix #2

--- direct.js
+++ direct.js
@@ -1,19 +1,54 @@
-(function() {
+(function(win) {
+    'use strict';
 
-document.addEventListener("DOMNodeInserted", function(e) {
-	if(typeof e.srcElement.getElementsByTagName == "undefined") return;
-        mousedown(e.srcElement.getElementsByTagName("A"));
-}, false);
+    var listeners = [],
+    doc = win.document,
+    MutationObserver = win.MutationObserver || win.WebKitMutationObserver,
+    observer;
 
-mousedown(document.getElementsByTagName("a"));
-
-
-function mousedown(element) {
-    for(var i=0; i<element.length; i++) {
-	if(element[i].target == "_blank") {
-		element[i].onmousedown = function() { return true; };
-	}
+    function ready(selector, fn) {
+        // Store the selector and callback to be monitored
+        listeners.push({
+            selector: selector,
+            fn: fn
+        });
+        if (!observer) {
+            // Watch for changes in the document
+            observer = new MutationObserver(check);
+            observer.observe(doc.documentElement, {
+                childList: true,
+                subtree: true
+            });
+        }
+        // Check if the element is currently in the DOM
+        check();
     }
-}
 
-})();
+    function check() {
+        // Check the DOM for elements matching a stored selector
+        for (var i = 0, len = listeners.length, listener, elements; i < len; i++) {
+            listener = listeners[i];
+            // Query for elements matching the specified selector
+            elements = doc.querySelectorAll(listener.selector);
+            for (var j = 0, jLen = elements.length, element; j < jLen; j++) {
+                element = elements[j];
+                // Make sure the callback isn't invoked with the
+                // same element more than once
+                if (!element.ready) {
+                    element.ready = true;
+                    // Invoke the callback with the element
+                    listener.fn.call(element, element);
+                }
+            }
+        }
+    }
+
+    // Expose `ready`
+    win.ready = ready;
+
+    ready('a', function(element) {
+        if (element.getAttribute('target') === "_blank") {
+            element.onmousedown = function() { element.setAttribute("data-lynx-uri", "");return true; };
+        }
+    });
+})(this);
