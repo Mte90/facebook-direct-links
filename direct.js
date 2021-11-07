d267f4587a0ac293fe716589d5cd07c3973f48ff
Peter Dave Hello <hsu@peterdavehello.org>
2021-11-07 16:59:13 +0800

    Add Facebook Tor onion v3 domain
    Reference:
    
    - https://www.facebook.com/facebookcorewwwi/posts/3727741430665883
    - https://www.facebook.com/onion-service
    - https://en.wikipedia.org/wiki/Facebook_onion_address

--- direct.js
+++ direct.js
@@ -81,10 +81,10 @@
         var url = element.href.toString();
         if (url === '') return;
 
-        var domainfilter= ['facebook.com', 'facebookwww.onion'];
+        var domainfilter= ['facebook.com', 'facebookwww.onion', 'facebookwkhpilnemxj7asaniu7vnjjbiltxjqhye3mhbshg7kx5tfyd.onion'];
         var domain = (new URL(url)).hostname.toString()
                         .split('.').slice(-2).join('.');
-        var trackerLinkRegex = /^https?:\/\/lm?.(facebook\.com|facebookwww\.onion)\/l.php\?u=([^&#$]+)/i;
+        var trackerLinkRegex = /^https?:\/\/lm?.(facebook\.com|facebookwww\.onion|facebookwkhpilnemxj7asaniu7vnjjbiltxjqhye3mhbshg7kx5tfyd\.onion)\/l.php\?u=([^&#$]+)/i;
 
         if( !domainfilter.includes(domain) || trackerLinkRegex.test(url) ) { //external links
             element.addEventListener('click', eventBlocker);
